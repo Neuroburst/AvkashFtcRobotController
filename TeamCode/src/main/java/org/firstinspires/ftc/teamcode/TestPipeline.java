8097d2f5a3f27fb332e931e7dbd0e4a3d9ae47c0
Neuroburst <69093127+Neuroburst@users.noreply.github.com>
2025-04-22 09:16:08 -0700

    added boxes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
@@ -12,14 +12,16 @@ import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.List;
 
-// TODO: add rects for items, and fix color stuff
+// TODO: need to adjust edge cutting
 
 public class TestPipeline extends OpenCvPipeline {
     String[] colorNames = {"Red", "Yellow", "Blue"};
     Scalar[] colorStyles = {new Scalar(255,0,0), new Scalar(255,255,0), new Scalar(0,0,255)};
 
     Scalar[] colorMax = {new Scalar(179, 255, 255), new Scalar(110, 255, 255), new Scalar(20, 255, 255)};
-    Scalar[] colorMin = {new Scalar(116, 100, 100), new Scalar(100, 100, 100), new Scalar(0, 0, 0)};
+    Scalar[] colorMin = {new Scalar(116, 100, 100), new Scalar(100, 110, 100), new Scalar(0, 0, 0)};
+
+    String outputText = "";
 
     @Override
     public void init(Mat input) {
@@ -35,20 +37,21 @@ public class TestPipeline extends OpenCvPipeline {
         // Blur it
         Imgproc.blur(input, input, new Size(2,2));
         Mat frame = input.clone();
+        Mat output = input.clone();
 
         // Cut out edges of objects using the difference of Gaussians filter
-        Mat DoG = new Mat();
-        Mat subInput3 = new Mat();
-        Imgproc.GaussianBlur(input, subInput3, new Size(3, 3), 0);
-        Mat subInput4 = new Mat();
-        Imgproc.GaussianBlur(input, subInput4, new Size(5, 5), 0);
-        Core.subtract(subInput3, subInput4, DoG);
-
-        Imgproc.cvtColor(DoG, DoG, Imgproc.COLOR_BGR2GRAY); // Convert Mask
-        Core.inRange(DoG, new Scalar(1, 1, 1), new Scalar(255, 255, 255), DoG);
-        Core.bitwise_not(DoG, DoG);
-        Imgproc.cvtColor(DoG, DoG, Imgproc.COLOR_GRAY2BGR); // Convert Mask
-        Core.bitwise_and(DoG, frame, frame);
+//        Mat DoG = new Mat();
+//        Mat subInput3 = new Mat();
+//        Imgproc.GaussianBlur(input, subInput3, new Size(3, 3), 0);
+//        Mat subInput4 = new Mat();
+//        Imgproc.GaussianBlur(input, subInput4, new Size(5, 5), 0);
+//        Core.subtract(subInput3, subInput4, DoG);
+//
+//        Imgproc.cvtColor(DoG, DoG, Imgproc.COLOR_BGR2GRAY); // Convert Mask
+//        Core.inRange(DoG, new Scalar(1, 1, 1), new Scalar(255, 255, 255), DoG);
+//        Core.bitwise_not(DoG, DoG);
+//        Imgproc.cvtColor(DoG, DoG, Imgproc.COLOR_GRAY2BGR); // Convert Mask
+//        Core.bitwise_and(DoG, frame, frame);
         //
 
         // Create HSV version
@@ -63,10 +66,7 @@ public class TestPipeline extends OpenCvPipeline {
 
 
         // Create blank output matrix
-        Mat debugOutput = new Mat();
-        debugOutput = Mat.zeros(frame.rows(), frame.cols(), input.type());
-
-        String outputText = "";
+        Mat debugOutput = Mat.zeros(frame.rows(), frame.cols(), input.type());
 
         // Configure parameters
         SimpleBlobDetector_Params params = new SimpleBlobDetector_Params();
@@ -80,7 +80,7 @@ public class TestPipeline extends OpenCvPipeline {
         params.set_filterByCircularity(false);
         params.set_filterByConvexity(false);
         params.set_filterByInertia(false);
-        params.set_minDistBetweenBlobs(50.0f);
+        params.set_minDistBetweenBlobs(50f);
 
         int idx = 0;
         for (String color : colorNames)
@@ -93,28 +93,48 @@ public class TestPipeline extends OpenCvPipeline {
             MatOfKeyPoint keyPoints = new MatOfKeyPoint();
             detector.detect(colorMask, keyPoints);
             List<MatOfPoint> contours = detector.getBlobContours();
+            for (MatOfPoint blob : contours){
+                Point[] extents = getContourExtents(blob);
+                Imgproc.rectangle(output, extents[0], extents[1], colorStyles[idx], 1);
+            }
 
             Features2d.drawKeypoints(debugOutput, keyPoints, debugOutput, colorStyles[idx]);
-            Imgproc.polylines(debugOutput, contours, true, colorStyles[idx]);
-            outputText = outputText.concat(color + ": " + Integer.toString(contours.size()) + " ");
+            Imgproc.drawContours(debugOutput, contours, -1, colorStyles[idx]);
+            outputText = outputText.concat(color + ": " + contours.size() + " ");
             idx++;
         }
 
         // Print Debug text
-        Imgproc.putText(debugOutput, outputText, new Point(0,20), 0, 0.75, new Scalar(255,0,0), 2);
+        Imgproc.putText(debugOutput, outputText, new Point(0,20), 0, 0.75, new Scalar(255,255,255), 2);
 
-////        Imgproc.rectangle(
-////                input,
-////                new Point(
-////                        input.cols()/4,
-////                        input.rows()/4),
-////                new Point(
-////                        input.cols()*(3f/4f),
-////                        input.rows()*(3f/4f)),
-////                new Scalar(0, 255, 0), 4);
-        Mat output = new Mat();
-        Core.add(input, debugOutput, output);
+        //Core.add(output, debugOutput, output);
         return output; // Return the image that will be displayed in the viewport
+    }
+
+    private Point[] getContourExtents(MatOfPoint contour){
+        Point[] points = contour.toArray();
+
+        double xMax = -1;
+        double xMin = -1;
+        double yMax = -1;
+        double yMin = -1;
+
+        for (Point point : points){
+            if (point.x > xMax || xMax == -1) {
+                xMax = point.x;
+            }
+            if (point.x < xMin || xMin == -1){
+                xMin = point.x;
+            }
+            if (point.y > yMax || yMax == -1){
+                yMax = point.y;
+            }
+            if (point.y < yMin || yMin == -1){
+                yMin = point.y;
+            }
+        }
+
+        return new Point[]{new Point(xMax, yMax), new Point(xMin, yMin)};
     }
 
     @Override

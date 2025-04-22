eaefe317b240264bebf0041b2b603118b0eb9164
Neuroburst <69093127+Neuroburst@users.noreply.github.com>
2025-04-21 22:07:13 -0700

    some code cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import org.opencv.core.*;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.features2d.Features2d;
 import org.opencv.features2d.SimpleBlobDetector;
@@ -28,78 +27,30 @@ public class TestPipeline extends OpenCvPipeline {
 
     @Override
     public Mat processFrame(Mat input) {
-        //Mat original = input.clone();
-        //Imgproc.blur(input, input, new Size(5, 5));
-
-        //Imgproc.Laplacian(input, input, 31);//CvType.CV_32F);
-
-
-//        Mat subInput1 = new Mat();
-//        Imgproc.GaussianBlur(original, subInput1, new Size(1, 1), 0);
-//        Mat subInput2 = new Mat();
-//        Imgproc.GaussianBlur(original, subInput2, new Size(51, 51), 0);
-//        Core.subtract(subInput1, subInput2, input);
-
-//        Mat DoH = new Mat();
-//        Imgproc.GaussianBlur(input, DoH, new Size(5, 5), 0);
-//        Mat Dxx = new Mat();
-//        Imgproc.Sobel(DoH, Dxx, CvType.CV_64F, 2, 0);
-//        Mat Dyy = new Mat();
-//        Imgproc.Sobel(DoH, Dyy, CvType.CV_64F, 0, 2);
-//        Mat Dxy = new Mat();
-//        Imgproc.Sobel(DoH, Dxy, CvType.CV_64F, 1, 1);
-//        Mat Mul = new Mat();
-//        Core.multiply(Dxx, Dyy, Mul);
-//        Core.multiply(Dxy, Dxy, Dxy);
-//        Core.subtract(Mul, Dxy, input);
-
-//        Mat subInput3 = new Mat();
-//        Imgproc.GaussianBlur(original, subInput3, new Size(5, 5), 0);
-//        Mat subInput4 = new Mat();
-//        Imgproc.GaussianBlur(original, subInput4, new Size(11, 11), 0);
-//        Core.subtract(subInput3, subInput4, input);
-        //Mat original = input.clone();
-        Mat inputHSV = new Mat();
-        Imgproc.cvtColor(input, inputHSV, Imgproc.COLOR_BGR2HSV);
-
-
+        // Convert colors otherwise it errors
+        Imgproc.blur(input, input, new Size(2,2));
+        Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2BGR);
         Imgproc.cvtColor(input, input, Imgproc.COLOR_BGR2RGB);
+        Mat frame = input.clone();
 
-        // First filter out everything that isn't very saturated
+        // Create HSV version
+        Mat inputHSV = new Mat();
+        Imgproc.cvtColor(frame, inputHSV, Imgproc.COLOR_BGR2HSV);
+
+        // Filter out everything that isn't very saturated
         Mat sat = new Mat();
         Core.inRange(inputHSV, new Scalar(0, 100, 45), new Scalar(179, 255, 255), sat);
         Imgproc.cvtColor(sat, sat, Imgproc.COLOR_GRAY2BGR); // Convert Mask
-        Core.bitwise_and(input, sat, input);
-        //Core.bitwise_and(inputHSV, sat, inputHSV);
+        Core.bitwise_and(frame, sat, frame);
 
 
+        // Create blank output mat
+        Mat debugOutput = new Mat();
+        Core.inRange(frame, new Scalar(255,255,255), new Scalar(0,0,0), debugOutput);
 
-//        // Now extract the individual colors
-//        Mat test = new Mat();
-//        Core.inRange(inputHSV, new Scalar(0, 100, 100), new Scalar(20, 255, 255), test);
-//        Imgproc.cvtColor(test, test, Imgproc.COLOR_GRAY2BGR); // Convert Mask
-//
-//        Core.bitwise_and(test, sat, test);
-//        Mat blue = new Mat();
-//        Core.inRange(inputHSV, new Scalar(0, 0, 0), new Scalar(20, 255, 255), blue);
-//        Imgproc.cvtColor(blue, blue, Imgproc.COLOR_GRAY2BGR); // Convert Mask
-//        Core.bitwise_and(blue, sat, blue);
+        String outputText = "";
 
-        // Next mask out
-//        Mat mask = new Mat();
-//        Core.inRange(input, new Scalar(10, 10, 10), new Scalar(255, 255, 255), mask);
-//        Imgproc.cvtColor(mask, mask, Imgproc.COLOR_GRAY2BGR); // Convert Mask
-//        Core.bitwise_not(mask, mask);
-//        Core.bitwise_and(input, mask, input);
-
-        // Finally convert it back
-        Imgproc.cvtColor(input, input, Imgproc.COLOR_RGB2BGR);
-
-        Mat output = new Mat();
-        // Create blank mat
-        Core.inRange(input, new Scalar(255,255,255), new Scalar(0,0,0), output);
-
-        // Configure params
+        // Configure parameters
         SimpleBlobDetector_Params params = new SimpleBlobDetector_Params();
         params.set_collectContours(true);
         params.set_filterByColor(false);
@@ -120,20 +71,19 @@ public class TestPipeline extends OpenCvPipeline {
             Core.inRange(inputHSV, colorMin[idx], colorMax[idx], colorMask);
             Imgproc.cvtColor(colorMask, colorMask, Imgproc.COLOR_GRAY2BGR); // Convert Mask
             Core.bitwise_and(colorMask, sat, colorMask);
-
             SimpleBlobDetector detector = SimpleBlobDetector.create(params);
             MatOfKeyPoint keyPoints = new MatOfKeyPoint();
             detector.detect(colorMask, keyPoints);
             List<MatOfPoint> contours = detector.getBlobContours();
 
-            Features2d.drawKeypoints(output, keyPoints, output, colorStyles[idx]);
-            Imgproc.polylines(output, contours, true, colorStyles[idx]);
-
+            Features2d.drawKeypoints(debugOutput, keyPoints, debugOutput, colorStyles[idx]);
+            Imgproc.polylines(debugOutput, contours, true, colorStyles[idx]);
+            outputText = outputText.concat(color + ": " + Integer.toString(contours.size()) + " ");
             idx++;
         }
 
-
-        //Imgproc.putText(input, Integer.toString(contours.size()) + " Blobs", new Point(0,20), 0, 0.75, new Scalar(255,0,0), 2);
+        // Print Debug text
+        Imgproc.putText(debugOutput, outputText, new Point(0,20), 0, 0.75, new Scalar(255,0,0), 2);
 
 ////        Imgproc.rectangle(
 ////                input,
@@ -144,7 +94,8 @@ public class TestPipeline extends OpenCvPipeline {
 ////                        input.cols()*(3f/4f),
 ////                        input.rows()*(3f/4f)),
 ////                new Scalar(0, 255, 0), 4);
-
+        Mat output = new Mat();
+        Core.add(input, debugOutput, output);
         return output; // Return the image that will be displayed in the viewport
     }
 
